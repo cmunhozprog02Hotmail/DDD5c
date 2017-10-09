b6864931fe06f60d194186b99dece01cb4043135
cmunhozprog <cmunhozprog02@hotmail.com>
2017-10-09 20:15:11 -0300

    Change EF, Class Categoria, Class Fornecedor, Criação Controller e views Index

---
+++ DDD5c/DDD5c/Controllers/FabricantesController.cs
@@ -0,0 +1,19 @@
+﻿using DDD5c.Models.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DDD5c.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(f => f.Nome));
+        }
+    }
+}
