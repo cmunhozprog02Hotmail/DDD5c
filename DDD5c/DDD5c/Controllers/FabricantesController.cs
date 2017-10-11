c300a4af681db46b6d42268733f2d2148b14cf4b
cmunhozprog <cmunhozprog02@hotmail.com>
2017-10-11 00:15:43 -0300

    Finalização Produto

--- DDD5c/DDD5c/Controllers/FabricantesController.cs
+++ DDD5c/DDD5c/Controllers/FabricantesController.cs
@@ -73,8 +73,8 @@ namespace DDD5c.Controllers
                 return new HttpStatusCodeResult(
                 HttpStatusCode.BadRequest);
             }
-            Fabricante fabricante = context.Fabricantes.
-            Find(id);
+            Fabricante fabricante = context.Fabricantes.Where(f => f.FabricanteId == id).
+                Include("Produtos.Categoria").First();
             if (fabricante == null)
             {
                 return HttpNotFound();
@@ -88,7 +88,7 @@ namespace DDD5c.Controllers
             if (id == null)
             {
                 return new HttpStatusCodeResult(
-                HttpStatusCode.BadRequest);
+                HttpStatusCode.BadRequest); 
             }
             Fabricante fabricante = context.Fabricantes.Find(id);
             if (fabricante == null)
