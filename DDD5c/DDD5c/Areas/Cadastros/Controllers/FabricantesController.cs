ba9efb34ee87d2282319cecba18d9c53b9b8125c
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-10-13 18:57:40 -0300

    Areas 02

---
+++ DDD5c/DDD5c/Areas/Cadastros/Controllers/FabricantesController.cs
@@ -0,0 +1,111 @@
+﻿using Modelo.Cadastros;
+using Servicos.Cadastros;
+using System.Net;
+using System.Web.Mvc;
+
+namespace DDD5c.Areas.Cadastros.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private FabricanteServico fabricanteServico = new FabricanteServico();
+
+        private ActionResult ObterVisaoFabricantePorId(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(
+                HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = fabricanteServico.ObterFabricantePorId((long)id);
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fabricante);
+        }
+
+        private ActionResult GravarFabricante(Fabricante fabricante)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    fabricanteServico.GravarFabricante(fabricante);
+                    return RedirectToAction("Index");
+                }
+                return View(fabricante);
+            }
+            catch
+            {
+                return View(fabricante);
+            }
+        }
+
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(fabricanteServico.ObterFabricantesClassificadosPorNome());
+        }
+
+        // GET: Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(Fabricante fabricante)
+        {
+            return GravarFabricante(fabricante);
+        }
+
+        // GET: Fabricantes/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            return ObterVisaoFabricantePorId(id);
+        }
+
+        // POST: Fabricantes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Fabricante fabricante)
+        {
+
+            return GravarFabricante(fabricante);
+        }
+
+        // GET: Testes/Details/5
+        public ActionResult Details(long? id)
+        {
+            return ObterVisaoFabricantePorId(id);
+        }
+
+        // GET: Fabricantes/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            return ObterVisaoFabricantePorId(id);
+        }
+
+        // POST: Fabricantes/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+                try
+                {
+                    Fabricante fabricante = fabricanteServico.EliminarFabricantePorId(id);
+                    TempData["Message"] = "Fabricante " + fabricante.Nome.ToUpper()
+                    + " foi removido";
+                    return RedirectToAction("Index");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+    }
+}
---
+++ DDD5c/DDD5c/Areas/Tabelas/Controllers/CategoriasController.cs
@@ -0,0 +1,114 @@
+﻿using Modelo.Tabelas;
+using Servicos.Tabelas;
+using System.Net;
+using System.Web.Mvc;
+
+
+namespace DDD5c.Areas.TabelasControllers
+{
+    public class CategoriasController : Controller
+    {
+        private CategoriaServico categoriaServico = new CategoriaServico();
+
+        private ActionResult ObterVisaoCategoriaPorId(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(
+                HttpStatusCode.BadRequest);
+            }
+            Categoria categoria = categoriaServico.ObterCategoriaPorId((long)id);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        private ActionResult GravarCategoria(Categoria categoria)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    categoriaServico.GravarCategoria(categoria);
+                    return RedirectToAction("Index");
+                }
+                return View(categoria);
+            }
+            catch
+            {
+                return View(categoria);
+            }
+        }
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(categoriaServico.ObterCategoriasClassificadasPorNome());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(Categoria categoria)
+        {
+            
+            return GravarCategoria(categoria);
+        }
+
+        // GET: Testes/Details/5
+        public ActionResult Details(long? id)
+        {
+            return ObterVisaoCategoriaPorId(id);
+        }
+
+        // GET: Categorias/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            return ObterVisaoCategoriaPorId(id);
+        }
+
+        // POST: Categorias/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categoria  categoria)
+        {
+            return GravarCategoria(categoria);
+        }
+
+        
+
+        // GET: Categorias/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            
+            return ObterVisaoCategoriaPorId(id);
+        }
+
+        // POST: Categoria/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            try
+            {
+                Categoria categoria = categoriaServico.EliminarCategoriaPorId(id);
+                TempData["Message"] = "Categoria " + categoria.Nome.ToUpper()
+                + " foi removido";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+
+}
