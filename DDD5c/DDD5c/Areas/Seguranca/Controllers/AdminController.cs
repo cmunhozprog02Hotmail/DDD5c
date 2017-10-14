b818d5ed563ab16f7ae99bf0cf9b81083a264be6
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-10-14 15:37:53 -0300

    Página 176 - 7.4

---
+++ DDD5c/DDD5c/Areas/Seguranca/Controllers/AdminController.cs
@@ -0,0 +1,63 @@
+﻿using System.Web.Mvc;
+using DDD5c.Infraestrutura;
+using System.Web;
+using Microsoft.AspNet.Identity.Owin;
+using DDD5c.Areas.Seguranca.Models;
+using Microsoft.AspNet.Identity;
+
+namespace DDD5c.Areas.Seguranca.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Seguranca/Admin
+        public ActionResult Index()
+        {
+            return View(GerenciadorUsuario.Users);
+        }
+        private GerenciadorUsuario GerenciadorUsuario
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().
+                GetUserManager<GerenciadorUsuario>();
+            }
+        }
+
+        // GET Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST Create
+        [HttpPost]
+        public ActionResult Create(UsuarioViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Usuario user = new Usuario
+                {
+                    UserName = model.Nome,
+                    Email = model.Email
+                };
+                IdentityResult result = GerenciadorUsuario.Create(user, model.Senha);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(model);
+        }
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+    }
+}
